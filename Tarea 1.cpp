e39e6abc0ac25c4f47ff7ffacbb021f8462377ef
dsanders123 <136214859+dsanders123@users.noreply.github.com>
2025-01-26 21:28:45 -0600

    Add files via upload

---
+++ Tarea 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+    double num1, num2, suma;
+    
+    cout << "Digite el primer numero: ";
+    cin >> num1;
+    
+    cout << "Digite el segundo numero: ";
+    cin >> num2;
+    
+    suma = num1 + num2;
+    cout << "La suma de " << num1 << " y " << num2 << " es: " << suma << endl;
+    
+    
+    
+    return 0;
+}
